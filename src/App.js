52ee63be6a8a866140fc2350358fd816618f5de0
mugishamatt <mugisham410@gmail.com>
2020-09-23 00:43:40 -0500

    added redux persist

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import './App.css';
 import Jumbo from './Components/Jumbo';
 import { Container } from 'react-bootstrap';
 import Layout from './Components/Layout';
+import {Provider} from 'react-redux';
+import {store,persistor} from './store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 
 
@@ -13,14 +17,18 @@ const App =() => {
  
   
     return (
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Layout>
       <div className="App">
 
       <Routes/>
       <Jumbo/>
-        
       </div>
+     
       </Layout>
+      </PersistGate>
+       </Provider> 
     );
   }
  
