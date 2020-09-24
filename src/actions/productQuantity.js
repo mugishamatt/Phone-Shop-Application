d1abb6696a81e0a4ef23b84b9855b56e5a42d4c4
mugishamatt <mugisham410@gmail.com>
2020-09-23 23:36:08 -0500

    formatted ducument

--- src/actions/productQuantity.js
+++ src/actions/productQuantity.js
@@ -1,26 +1,26 @@
-import {INCREASE_QUANTITY,DECREASE_QUANTITY,CLEAR_PRODUCT} from './types';
+import { INCREASE_QUANTITY, DECREASE_QUANTITY, CLEAR_PRODUCT } from './types';
 
-export const productQuantity= (action,name)=>{
-    return(dispatch)=>{
+export const productQuantity = (action, name) => {
+    return (dispatch) => {
         console.log("inside product Quantity");
-        console.log("the action is",action);
+        console.log("the action is", action);
         console.log("the product name is", name);
         dispatch({
-            type: action === "encrease" ? INCREASE_QUANTITY :DECREASE_QUANTITY,
-            payload:name
+            type: action === "encrease" ? INCREASE_QUANTITY : DECREASE_QUANTITY,
+            payload: name
         })
     }
-   
+
 }
 
-export const clearProduct =(name)=>{
-    return(dispatch)=>{
+export const clearProduct = (name) => {
+    return (dispatch) => {
         console.log("inside clear product");
-        console.log("product Name",name);
+        console.log("product Name", name);
 
         dispatch({
-            type:CLEAR_PRODUCT,
-            payload:name
+            type: CLEAR_PRODUCT,
+            payload: name
 
         })
     }
